2bfad2d309ea46cd439908490b3da2ae4f2bc6b9
ph-1dnjs <ph.1dnjs@gmail.com>
2025-05-11 19:04:36 +0900

    feat: fetchHolidays 단위 테스트 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,35 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date('2025-01-15'); // 1월
+    const result = fetchHolidays(date);
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    expect(result).toEqual({
+      '2025-01-01': '신정',
+      '2025-01-29': '설날',
+      '2025-01-30': '설날',
+      '2025-01-31': '설날',
+    });
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date('2025-02-01'); // 2025년 2월 (공휴일 없음)
+    const result = fetchHolidays(date);
+
+    expect(result).toEqual({});
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date('2025-10-05');
+    const result = fetchHolidays(date);
+
+    expect(result).toEqual({
+      '2025-10-03': '개천절',
+      '2025-10-05': '추석',
+      '2025-10-06': '추석',
+      '2025-10-07': '추석',
+      '2025-10-09': '한글날',
+    });
+  });
 });
